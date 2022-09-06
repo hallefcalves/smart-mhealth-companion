07d1a6522b76ce846b45967239d325d930107376
hallefalves <hallef28@gmail.com>
2022-09-06 19:25:32 -0300

    json starting

--- lib/HTTP/web.dart
+++ lib/HTTP/web.dart
@@ -51,7 +51,7 @@ sendData() async {
 ?todos> ultimaAtualizacao:data
 
 remedios: {
-    id: int,
+    id: string,
     nome: "",
     img: "",
     lote: "",
@@ -60,16 +60,43 @@ remedios: {
     codigo: ref,
 }
 
+{
+    "id": "urn:ngsi-ld:remedio:001",
+    "type": "remedio",
+    "name": {"type": "string", "value": "Advil"},
+    "imagem": {
+      "type": "string",
+      "value":
+          "https://www.erifarma.com.br/medicamentos/advil-400mg-20-capsulas"
+    },
+    "lote": {"type": "Text", "value": "AR750"},
+    "qtdPilulas": {"type": "Integer", "value": 8},
+    "dataValidade": {"type": "date", "value": "22/07/2029"},
+    "refIdoso": {"type": "Relationship", "value": "urn:ngsi-ld:Idoso:001"}
+  }
+
 alarme: {
-    id: int,
-    horario_inicio:ref,
+    id: string,
+    horario_inicio: time,
     frequencia: ref,
     remedio:ref,
     codigo: ref,
 }
 
+{
+    "id": "urn:ngsi-ld:alarme:001",
+    "type": "alarme",
+    "name":{
+      "type": "Text"
+      "value": "8 em 8"
+    }
+    "horario_inicio": "08:11 UTC-3"
+    "remedio": {"type":"Relationship", "value":"urn:ngsi-ld:remedio:001"}
+    "codigo":  {"type":"Relationship", "value":"102536"}
+}
+
 ingestaoRemedios {
-    id: int,
+    id: string,
     remedio: ref,
     dataHora: timestamp,
     tentativasAtraso: int,
@@ -78,7 +105,7 @@ ingestaoRemedios {
 } 
 
 idoso {
-  id: int,
+  id: string,
   refRemedio: [],
   refAlarme: [],
   refIngestaoRemedios: [],
